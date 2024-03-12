a64447340fc22a90b1abf6b18432dd0aadfc8fd7
naysolange <naylaportas@gmail.com>
2024-03-12 11:55:59 -0300

    Genera json solo con los reportes

---
+++ src/scripts/generate_reports_json.js
@@ -0,0 +1,40 @@
+import * as fs from "fs";
+import { readFileSync } from "fs";
+
+const reportJsonPath = "../data/raw/transparency_report.json";
+
+try {
+  const reportJsonContent = readFileSync(reportJsonPath, "utf-8");
+  const reportJsonData = JSON.parse(reportJsonContent);
+
+  const processedReports = [];
+
+  reportJsonData.forEach((element) => {
+    const report = {
+      title: element.fraccion_nombre_del_documento,
+      description: element.descripcion,
+      link: element.documentos[0],
+      year: element.ano_de_inicio || element.ano_de_finalizacion,
+      month: element.mes_de_inicio || element.mes_de_finalizacion,
+      date: element.fecha
+    };
+    processedReports.push(report);
+  });
+
+  writeFile(processedReports);
+} catch(error) {
+    console.error("Error parsing file:", error.message);
+}
+
+
+function writeFile(processedJson) {
+    const filePath = "../data/processed/reports.json";
+
+    try {
+        const jsonString = JSON.stringify(processedJson, null, 2);
+        fs.writeFileSync(filePath, jsonString, "utf-8");
+        console.log(`File ${filePath} created successfully.`);
+    } catch (err) {
+        console.error("Error writing file:", err);
+    }
+}
