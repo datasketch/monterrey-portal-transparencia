208076a2c1053d7ab816d12b74b5afdf4fffa64b
naysolange <naylaportas@gmail.com>
2024-03-21 11:16:27 -0300

    Agrega reportes obtenidos del scrapping para la busqueda

--- src/scripts/generate_reports_json.js
+++ src/scripts/generate_reports_json.js
@@ -2,16 +2,27 @@ import * as fs from "fs";
 import { readFileSync } from "fs";
 
 const reportJsonPath = "../data/raw/transparency_report.json";
+const dataFullScrappingPath = "../data/scrapped/dataFullScrapping.json";
+const dataScrappingByLabelPath = "../data/scrapped/dataScrappingByLabel.json";
 
 try {
   const reportJsonContent = readFileSync(reportJsonPath, "utf-8");
   const reportJsonData = JSON.parse(reportJsonContent);
+  const dataScrappingByLabelJsonContent = readFileSync(dataScrappingByLabelPath, "utf-8");
+  const dataScrappingByLabelJsonData = JSON.parse(dataScrappingByLabelJsonContent);
+  const dataFullScrappingJsonContent = readFileSync(dataFullScrappingPath, "utf-8");
+  const dataFullScrappingJsonData = JSON.parse(dataFullScrappingJsonContent);
 
   const processedReports = [];
 
   reportJsonData.forEach((element) => {
+    let title = element.fraccion_nombre_del_documento;
+    if(!element.fraccion_nombre_del_documento && element.documentos[0]) {
+      const fileName = element.documentos[0].split("/").pop();
+      title = fileName.split(".")[0];
+    }
     const report = {
-      title: element.fraccion_nombre_del_documento,
+      title: title,
       description: element.descripcion,
       link: element.documentos[0],
       year: element.ano_de_inicio || element.ano_de_finalizacion,
@@ -32,6 +43,18 @@ try {
       processedReports.push(report);
     }
     
+  });
+
+  dataScrappingByLabelJsonData.forEach((element) => {
+    if(element) {
+      processedReports.push(...element.documentos);
+    }
+  });
+
+  dataFullScrappingJsonData.forEach((element) => {
+    if(element) {
+      processedReports.push(...element.documentos);
+    }
   });
 
   writeFile(processedReports);
