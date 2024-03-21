667c9e437c165a04f8517fbb23464e6d2614c609
yoshi <yosman@datasketch.co>
2024-03-21 15:10:13 -0500

    update: add reports

--- src/router/index.js
+++ src/router/index.js
@@ -3,63 +3,47 @@ import HomeView from '../views/HomeView.vue';
 import jsonData from '@/data/processed/structure.json';
 import slugify from 'slugify';
 
-// function generateRoutes(data, prefix) {
-//   return data.map((item) => {
-//     const path = `${prefix}/${slugify(item.label, { lower: true })}`;
-//     const routeName = slugify(path, { strict: true })
-//     return {
-//       path,
-//       name: routeName,
-//       component: () => import('@/views/CategoryView.vue'),
-//       props: {
-//         id: item.id,
-//         label: item.label,
-//         // routeName,
-//         children:
-//           (item.children &&
-//             item.children.reduce((prev, { id, label, reports }) => {
-//               return [...prev, { id, label, reports }];
-//             }, [])) ||
-//           [],
-//       },
-//       children: item.children ? generateRoutes(item.children, path) : [],
-//     };
-//   });
-// }
-
-const toSlug = (label) => slugify(label, { lower: true })
+const toSlug = (label) => slugify(label, { lower: true });
 
 function generateRoutes(data, prefix = '', bc = []) {
   return data.reduce((result, item) => {
-    const slug = toSlug(item.label)
+    const slug = toSlug(item.label);
     const path = `${prefix}/${slug}`;
-    const breadcrumb = [...bc, { label: item.label, slug: path }]
-    const set = [{
-      path,
-      component: () => import('@/views/CategoryView.vue'),
-      props: {
-        id: item.id,
-        label: item.label,
-        breadcrumb,
-        items: item.children && Array.isArray(item.children) ? item.children.reduce((prev, { id, label, reports }) => {
-          return [...prev,
-            { id, label, reports, slug: `${path}/${toSlug(label)}` }
-          ];
-        }, []) : []
-      }
-    }]
-    if (item.children && Array.isArray(item.children) && item.children.length > 0) {
-      set.push(...generateRoutes(item.children, path, breadcrumb))
+    const breadcrumb = [...bc, { label: item.label, slug: path }];
+    const reports = item.reports || [];
+    const set = [
+      {
+        path,
+        component: () => import('@/views/CategoryView.vue'),
+        props: {
+          id: item.id,
+          label: item.label,
+          breadcrumb,
+          reports,
+          items:
+            item.children && Array.isArray(item.children)
+              ? item.children.reduce((prev, { id, label, reports }) => {
+                  return [
+                    ...prev,
+                    { id, label, reports, slug: `${path}/${toSlug(label)}` },
+                  ];
+                }, [])
+              : [],
+        },
+      },
+    ];
+    if (
+      item.children &&
+      Array.isArray(item.children) &&
+      item.children.length > 0
+    ) {
+      set.push(...generateRoutes(item.children, path, breadcrumb));
     }
-    return [
-      ...result,
-      ...set
-    ]
-  }, [])
+    return [...result, ...set];
+  }, []);
 }
 
 const routes = generateRoutes(jsonData, '');
-console.log(routes);
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
--- src/views/CategoryView.vue
+++ src/views/CategoryView.vue
@@ -4,7 +4,7 @@
       <BreadCrumbs :items="breadcrumb" />
       <div class="mt-8 md:mt-12 lg:mt-16">
         <div class="flex flex-col md:flex-row gap-y-8 gap-x-16">
-          <div class="block w-full md:max-w-[347px]">
+          <div v-if="reports.length === 0" class="block w-full md:max-w-[347px]">
             <div class="bg-indigo-dye p-4 rounded-[5px]">
               <CategoryNavigation :items="items" />
             </div>
@@ -13,8 +13,15 @@
             <h2 class="text-2xl lg:text-3xl font-semibold first-letter:uppercase">
               {{ label }}
             </h2>
-            <div class="mt-12">
+            <div v-if="reports.length > 0" class="mt-12">
+              <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+                <ReportCard v-for="(report, i) in reports" :key="`report-${i + 1}`" :report="report" />
+              </div>
             </div>
+            <p v-else class="mt-4 text-sm">
+              Da clic en alguna de las opciones de la izquierda, para ver los
+              reportes asociados.
+            </p>
           </div>
         </div>
       </div>
@@ -25,9 +32,10 @@
 <script>
 import BreadCrumbs from '@/components/BreadCrumbs.vue';
 import CategoryNavigation from '@/components/CategoryNavigation.vue';
+import ReportCard from '@/components/ReportCard.vue';
 
 export default {
-  components: { BreadCrumbs, CategoryNavigation },
+  components: { BreadCrumbs, CategoryNavigation, ReportCard },
   props: {
     id: {
       type: String,
@@ -42,12 +50,9 @@ export default {
     },
     breadcrumb: {
       type: Array
-    }
-  },
-  data() {
-    return {
-      categoryObj: this.$route.matched[this.$route.matched.length - 1],
-      matches: this.$route.matched
+    },
+    reports: {
+      type: Array
     }
   }
 }
